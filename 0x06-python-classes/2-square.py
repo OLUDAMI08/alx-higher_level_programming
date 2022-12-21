8decbf18a31db9821bc49f47242ed61249908cad
lawal toheeb damilare <lawaltd088@gmail.com>
2022-12-21 18:36:34 +0000

    class with a protected attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
         size(int) - size of the square
     """
 
-        if not isintance(size, int):
+       if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
