4cd87b28c6a767f061baeac40f13c4bd66c71c72
lawal toheeb damilare <lawaltd088@gmail.com>
2022-12-21 18:34:13 +0000

    class with a protected attribute

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""class that define a square"""
+
+class Square:
+    """Define a class square"""
+
+    def __init___(self, size=0):
+
+        """initializing a class
+
+    Args:
+        size(int) - size of the square
+    """
+
+        if not isintance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
