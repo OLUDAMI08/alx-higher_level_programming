a31af92436c27e38e3c3a9e22fb56bf0d101ddc6
lawal toheeb damilare <lawaltd088@gmail.com>
2022-12-21 17:56:47 +0000

    initializing a square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""class that define a square"""
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size):
+        """initializing the square class
+        Args: size - represent the size of the square defined
+        """
+
+        self.__size = size
