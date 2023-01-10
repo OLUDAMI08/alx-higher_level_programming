de48132df7c92c4cebb118b6d8ae70509d6c5c8a
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-10 12:33:50 +0000

    function that returns attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""This module returns the list of available attributes and methods of an object"""
+
+def lookup(obj):
+
+    """This function returns the available attributes and method"""
+    return dir(obj)
