b526ebbc66a21e4739518de83e1aedfa159f88e6
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-10 21:04:17 +0000

    function that compare class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ an instance of the specified class ; otherwise False."""
 def is_same_class(obj, a_class):
     """function that return true if the obj is an instance of a class"""
 
-    return isinstance(obj, a_class)
+    return (type(obj) is a_class)
