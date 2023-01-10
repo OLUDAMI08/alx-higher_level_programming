457cc8c72c16d96e8dcfb1115adba5feba88c398
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-10 20:54:06 +0000

    funtion that inherit a list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,8 +2,12 @@
 
 """This module inherit from the list class"""
 
+
 class MyList(list):
     """A class that inherit from list"""
+
+
     def print_sorted(self):
         """A method to print a sorted list"""
+
         print(sorted(self))
