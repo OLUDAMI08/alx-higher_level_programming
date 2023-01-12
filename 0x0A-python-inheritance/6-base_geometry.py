7c34060de1e2365f3ea11892d84ff7b9cc4bb6fb
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-12 18:46:00 +0000

    a class with a method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class BaseGeometry module"""
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Area not implemented yet"""
+
+        raise Exception("area() is not implemented")
