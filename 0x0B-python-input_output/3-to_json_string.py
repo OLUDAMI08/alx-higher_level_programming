f4093863a2e07d80749b9f02ed40900ea4bb03fc
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-14 17:08:27 +0000

    function that serialize

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that returns the JSON
+representation of an object (string)
+"""
+
+def to_json_string(my_obj):
+    """function that accept an argument"""
+
+    return json.dumps(my_obj)
