0cab0d95c16969a020a1cc04b005469bcce4c276
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-14 16:10:23 +0000

    function that write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,4 @@ characters written"""
 
 def write_file(filename="", text=""):
     with open(filename, "w", encoding="utf-8") as f:
-       my_text =  f.write(text)
-       return my_text
+       return f.write(text)
