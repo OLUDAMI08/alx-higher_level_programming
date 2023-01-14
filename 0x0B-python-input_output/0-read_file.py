02390007e4d28613af132dfbc3fca8542fe56b7a
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-14 15:57:14 +0000

    function that read to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,9 @@ def read_file(filename=""):
     """A function that accept an argument
 
     Args:
-        filename: the file that is passed
-        as an argument"""
+        filename(str): the file that is passed
+        as an argument
+    """
         with open(filename, "r", encoding="utf-8") as f:
             print(f.read(), end="")
 
