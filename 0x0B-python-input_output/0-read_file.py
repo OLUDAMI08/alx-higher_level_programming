cfa4873b9468621e87b1bea47797e42cfa5586dc
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-14 15:45:44 +0000

    function that read to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A m function that read a text file stdout"""
+
+
+def read_file(filename=""):
+    """A function that accept an argument
+
+    Args:
+        filename: the file that is passed
+        as an argument"""
+        with open(filename, 'r', encoding = utf-8) as f:
+            print(f.read(), end="")
+
