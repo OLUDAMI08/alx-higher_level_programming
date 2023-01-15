3fc7c85c2fd640a4452c52ffad621f608b9f4900
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-15 11:30:32 +0000

    function that return dict of json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ description with simple data structure
 (list, dictionary, string, integer and boolean)
 for JSON serialization of an object"""
 
+
 def class_to_json(obj):
     """Return the dictionary description of an object
     for JSON serialization"""
