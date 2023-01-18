b860124dd577317009e20e36fccb3a6776519d9b
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-18 16:06:48 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,10 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = id
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
