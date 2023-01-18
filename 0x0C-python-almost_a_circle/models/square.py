74166e7582362a1fa51fa6d2a47cd7ed9a3d259c
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-18 15:59:30 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,15 +15,15 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        """getter method"""
+        """get the value for withd"""
         return self.__width
 
     @size.setter
     def size(self, value):
-        """setter method"""
+        """set the value for width"""
         if type(value) is not int:
-            raise TypeError(width must be an integer)
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError(width must be > 0)
+            raise ValueError("width must be > 0")
         self.__width = value
         self.__height = value
