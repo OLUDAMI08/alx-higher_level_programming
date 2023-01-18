48a4acde894c89c7515aff3d3ce2bf50fa9cab5f
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-18 15:46:35 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""square class that inherit from rectangle"""
-
+"""This module contains a square class"""
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """square class"""
+    """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
-        super()__init__(self, size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
-    def __str___(self):
-        """format for string representation"""
-        return f"[Square] {self.id} {self.x}/{self.y} - {self.id}"
+    def __str__(self):
+        """Defines a format for the string representation of the class"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
