b75c84bb788050c2da7dcb306568ca4833b5b82b
lawal toheeb damilare <lawaltd088@gmail.com>
2023-02-16 13:23:50 +0000

    update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,4 @@
 -- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
-
 -- states description:
 --id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
