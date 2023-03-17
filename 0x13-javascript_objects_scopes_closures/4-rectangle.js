d559515d6489695e3ac37b0ec9c0affcd6678a15
lawal toheeb damilare <lawaltd088@gmail.com>
2023-03-17 18:36:57 +0000

    javascript classes

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,29 @@
 #!/usr/bin/node
-
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 
-  print () {
-    for (let i = 1; i <= this.height; i++) {
-      console.log('X'.repeat(this.width));
+  print (ch = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(ch.repeat(this.width));
     }
   }
 
   rotate () {
-	  
-	  // alternative method
-    const newHeight = this.width;
+    /* alternative solution 1
+    const temp = this.width;
     this.width = this.height;
-    this.height = newHeight;
-
-   //  [this.width, this.height] = [this.height, this.width];
+    this.height = temp;
+    alternative solution 2
+    this.width = this.width ^ this.height;
+    this.height = this.width ^ this.height;
+    this.width = this.width ^ this.height; */
+    // 1 liners :)
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
@@ -30,5 +31,4 @@ class Rectangle {
     this.height *= 2;
   }
 }
-
 module.exports = Rectangle;
