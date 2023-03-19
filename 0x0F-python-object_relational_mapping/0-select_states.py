ac0319367e5ed9d7769f4ed6200c5009cefd14ec
lawal toheeb damilare <lawaltd088@gmail.com>
2023-03-19 10:18:45 +0000

    relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,19 +3,18 @@
 import sys
 import MySQLdb
 
-if __name__ = "__main__"
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db_connect = MySQLdb.connect(host = "localhost",
-            port = 3306, 
-            user=username, 
-            passwd=password, 
-            db=dd_name)
+    db_connect = MySQLdb.connect(host="localhost", port=3306, user=username,
+    \passwd=password, db=dd_name)
     db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states")
+    db_cursor.execute("SELECT * FROM states ORDER BY states.id")
     datas_fetched = db_cursor.fetchall()
     for data in datas_fetched:
         print(data)
 
+    db_cursor.close()
+    db_connect.close()
