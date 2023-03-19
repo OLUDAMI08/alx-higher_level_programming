a968e643e1d9ba867af1c02547817c3d956163c2
lawal toheeb damilare <lawaltd088@gmail.com>
2023-03-19 12:01:10 +0000

    relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     running on localhost at port 3306"""
     db_connect = MySQLdb.connect(host="localhost", port=3306,
                                  user=sys.argv[1], passwd=sys.argv[2],
-                                 db=sys.argv[3], state_name[4])
+                                 db=sys.argv[3], state_name=sys.argv[4])
     """ Prepare a cursor object"""
     db_cursor = db_connect.cursor()
 
