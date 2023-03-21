3b0e5edbf46ee110cd89b1a2523aa67bff5ae310
lawal toheeb damilare <lawaltd088@gmail.com>
2023-03-21 13:52:17 +0000

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" a script that lists all states with a name starting with N (upper N)
-    from the database hbtn_0e_0_usa
+
+"""script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
 """
 import sys
 import MySQLdb
