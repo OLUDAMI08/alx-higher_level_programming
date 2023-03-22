df00291882c832c3c31eb227c8e12d018863c021
lawal toheeb damilare <lawaltd088@gmail.com>
2023-03-22 16:16:39 +0000

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-"""This script defines a State class and an instance"""
+""" Defines a State class
+    Inherits from SQLAlchemy Base and links to the MySQL table states
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,10 +10,11 @@ Base = declarative_base()
 
 
 class State(Base):
-    """ State class represents a state table in a MySQL database. """
-    __tablename__ = "state"
-    
-    """table column"""
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False, unique=True)
+    """ Represents a state column for a MySQL table
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
