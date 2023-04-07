a88ca6c38300e9ca0dd4ebe6b3aed212063afbdf
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-07 01:09:41 -0700

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST -d "subject=I will always be here for PLD & email=test@gmail.com" "$1"
+curl -s -X POST -d "subject=I will always be here for PLD & email=test@gmail.com" "$1"
