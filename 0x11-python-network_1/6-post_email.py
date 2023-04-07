a7707415074d1f263be9236e79d500bc1eb48572
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-07 21:13:47 +0100

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,10 +8,12 @@ import requests
 import sys
 
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-payload = {'email': email}
-response = requests.post(url, data=payload)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-print(response.text)
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    print(response.text)
