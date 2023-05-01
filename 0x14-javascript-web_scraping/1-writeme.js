e65c44bbfdbb9bc58b4aba44c177ee66c0659171
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-05-01 20:04:31 +0100

    script that write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*script that writes a string to a file*/
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(filePath, data, 'utf-8', error => {
+  if (error) {
+    console.log(error);
+  }
+});
